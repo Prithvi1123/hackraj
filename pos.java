a407f5201686704397e8a3148eaffbe8b7ddfd45
Prithvi1123 <42169837+Prithvi1123@users.noreply.github.com>
2018-08-07 14:24:47 +0530

    Update pos.java

--- pos.java
+++ pos.java
@@ -1,2 +1,24 @@
-if a<0
-sy(negative)
+class pos
+{
+  public static void main(String args[])
+  {
+    int a=3;
+    if(a>0)
+    {
+      System.out.println("positive");
+    }
+   else if(a<0)
+   {
+ System.out.println("negative");
+   }
+   else if(a=0)
+   {
+      System.out.println("Zero");
+   }
+   else
+   {
+      System.out.println("invalid");
+   }
+  }
+}
+     
