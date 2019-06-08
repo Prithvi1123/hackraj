b60bb7ccbea756620343f6161a8565128c2ed547
Prithvi1123 <42169837+Prithvi1123@users.noreply.github.com>
2019-06-08 14:36:18 +0530

    Update guvi.py

--- guvi.py
+++ guvi.py
@@ -1,11 +1,11 @@
 # hackraj
-value1,value2=map(int,input().split())
+a,b=map(int,input().split())
 l=list(map(int,input().split()))
 result=1
 for i in range(0,len(l)):
 	for j in range(i+1,len(l)):
 		m=l[i]+l[j]
-		if m==value2:
+		if m==b:
 			result=0
 if result==0:
 	print("yes")
